87ede68f73655042bb3dc421de01a6923061bc26
Tracy Reuther <tracyreuther@gmail.com>
2019-08-11 23:51:31 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,15 @@
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+  
+  def self.create_table
+    
+  end
 end
