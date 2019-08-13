ec2b0728f5a7fcbdcd4fb1d4b623db8397a47704
Tracy Reuther <tracyreuther@gmail.com>
2019-08-13 21:12:11 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,13 +3,30 @@ class Pokemon
   attr_accessor :id, :name, :type, :db
   
   def initialize(id:, name:, type:, db:)
-    @id = id
     @name = name
     @type = type
+    @id = id
     @db = db
   end
   
-  def self.create_table
-    
+  def self.save(name, type)
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO pokemon (name, type, id)
+        VALUES (?, ?)
+        SQL
+      
+      DB[:conn].execute(sql, self.name, self.type, self.id)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
   end
+  
+  def update
+    sql = "UPDATE pokemon SET name = ?, type = ?, WHERE id = ?"
+    
+    DB[:conn].execute(sql, self.name, self.type, self.id)
+  end
+  
 end
