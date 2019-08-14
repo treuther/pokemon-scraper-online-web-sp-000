9dafb14acac8ec1b9bad7ee3548740b765290a07
Tracy Reuther <tracyreuther@gmail.com>
2019-08-14 22:33:15 +0000

    Done.

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -2,5 +2,12 @@
 -- It should create a table in a new table in the db/ directory.
 
 def self.create_table
-
+  sql = <<-SQL
+    CREATE TABLE IF NOT EXIST pokemon (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      type TEXT,
+    )
+  SQL
+  
 end
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,24 +9,24 @@ class Pokemon
     @db = db
   end
   
-  def self.save(name, type)
-    if self.id
-      self.update
-    else
-      sql = <<-SQL
-        INSERT INTO pokemon (name, type)
-        VALUES (?, ?)
-        SQL
+#   def self.save(name, type)
+#     if self.id
+#       self.update
+#     else
+#       sql = <<-SQL
+#         INSERT INTO pokemon (name, type)
+#         VALUES (?, ?)
+#         SQL
       
-      DB[:conn].execute(sql, self.name, self.type, self.id)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
-    end
-  end
+#       DB[:conn].execute(sql, self.name, self.type, self.id)
+#       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+#     end
+#   end
   
-  def update
-    sql = "UPDATE pokemon SET name = ?, type = ?, WHERE id = ?"
+#   def update
+#     sql = "UPDATE pokemon SET name = ?, type = ?, WHERE id = ?"
     
-    DB[:conn].execute(sql, self.name, self.type, self.id)
-  end
+#     DB[:conn].execute(sql, self.name, self.type, self.id)
+#   end
   
 end
